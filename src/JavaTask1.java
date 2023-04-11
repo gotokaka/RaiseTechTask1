b90d59999859920ea199807ea60d180ffc9f2791
gotokaka <kakaporider510@gmail.com>
2023-04-11 17:20:33 +0900

    第一回課題

---
+++ src/JavaTask1.java
@@ -0,0 +1,11 @@
+public class JavaTask1 {
+    public static void main(String[] args) {
+                for(int i=1;i<=20;i++) {
+                    if(i<10) {
+                        System.out.println("hello World!");
+                    }else {
+                        System.out.println("hello!");
+                    }
+                }
+    }
+}
---
+++ src/JavaTask1_1.java
@@ -0,0 +1,12 @@
+
+  public class JavaTask1_1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 15; i++) {
+            if (i <= 10) {
+                System.out.println("ココア");
+            } else {
+                System.out.println("コーヒー");
+            }
+        }
+    }
+  }
