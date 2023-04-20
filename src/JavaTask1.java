6e70d7108cf530a6fd37b144835b6b2c230f22d3
gotokaka <kakaporider510@gmail.com>
2023-04-20 21:14:00 +0900

    4行目の変更

--- src/JavaTask1.java
+++ src/JavaTask1.java
@@ -1,7 +1,7 @@
 public class JavaTask1 {
     public static void main(String[] args) {
                 for(int i=1;i<=20;i++) {
-                    if(i<12) {
+                    if(i<15) {
                         System.out.println("hello World!");
                     }else {
                         System.out.println("hello!");
