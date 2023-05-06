87457be48a7cb11959ffaae1503f6983f6ddb3a7
gotokaka <kakaporider510@gmail.com>
2023-05-06 14:43:38 +0900

    上位ディレクトリの名称変更

---
+++ src/JavaTest.java
@@ -0,0 +1,17 @@
+public class JavaTest {
+    /*
+    public static void main(String[] args) {
+        int a = 10;
+        int b = a++;
+        System.out.println(b);
+        System.out.println(a);
+    }*/
+
+    public static void main(String[] args) {
+        int c = 10;
+        int d = c++;
+        int e = ++c;
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
