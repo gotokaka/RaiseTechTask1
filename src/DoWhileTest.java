1d39d0bf325c11519b0076ab96447c59e03cf8dc
gotokaka <kakaporider510@gmail.com>
2023-05-06 19:38:22 +0900

    `DoWhileTest`クラスの追加
    Java SE Bronzeの基礎学習において、教材の解説の挙動を確認する為に作成したクラス。

---
+++ src/DoWhileTest.java
@@ -0,0 +1,10 @@
+//教材学習の挙動確認メモ//
+public class DoWhileTest {
+  public static void main(String[] args) {
+    int i = 1;
+    do {
+      i = i + 1;
+    } while (i < 10);
+    System.out.println(i);
+  }
+}
